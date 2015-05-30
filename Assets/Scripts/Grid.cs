e74e5ca5b6c471c68ec1e3e4ec39698fb222ca32
Kevin Xing <kxing@uwaterloo.ca>
2015-05-30 14:36:42 -0400

    Spawner complete, Grid initial script

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+	// The Grid itself
+	public static int w = 24;
+	public static int h = 12;
+	public static Transform[,] grid = new Transform[w, h];
+}
