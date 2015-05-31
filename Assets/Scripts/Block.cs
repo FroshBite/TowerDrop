82ef8c427d2b22018d42b69d4878a1eeddabe3be
Lucas Zeer-Wanklyn <lucas.zeer@gmail.com>
2015-05-30 22:10:40 -0400

    tetris stuff

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -8,10 +8,12 @@ public class Block : MonoBehaviour {
 	void destroy(){
 		Destroy(this.gameObject);
 	}
-	bool isDestroyed(){ //checks if the block was destroyed
+	bool isDestroyed(){ 
+		//checks if the block was destroyed
+
 		if (health <= 0) {
 			GameObject death = GameObject.Instantiate(deathAnimation);
-
+			
 			for (int i= 0; i < gameObject.transform.parent.gameObject.transform.childCount; i++){
 				GameObject child = gameObject.transform.parent.gameObject.transform.GetChild (i).gameObject;
 				child.GetComponent<Rigidbody2D>().isKinematic = false;
@@ -21,14 +23,17 @@ public class Block : MonoBehaviour {
 			destroy ();
 			return true;
 		}
+
 		return false;
 	}
+
 	public void takeDamage(int ammount){
 		if (ammount > 0) {
 			health -= ammount;
 		}
 		print ("Take Damage Called");
 	}
+
 	// Use this for initialization
 	void Start () {
 
